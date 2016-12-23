95a9ef28566393dd1b527cd7189a54808486476a
Heikki Pora <heikki.pora@reaktor.fi>
2016-12-23 07:54:00 +0200

    Refactor config

--- src/config.js
+++ src/config.js
@@ -6,8 +6,11 @@ const yaml = require('js-yaml')
 const CONFIG_FILE = expandTilde(process.env.GITLAB_RADIATOR_CONFIG || '~/.gitlab-radiator.yml')
 const CONFIG_POLL_INTERVAL_SEC = process.env.GITLAB_RADIATOR_CONFIG_POLL_INTERVAL_SEC || 60
 
-function expandTilde(path) {
-  return path.replace(/^~($|\/|\\)/, `${os.homedir()}$1`)
+function pollConfig(interval) {
+  return Bacon.later(0, true)
+    .merge(Bacon.interval(CONFIG_POLL_INTERVAL_SEC * 1000, true))
+    .map(CONFIG_FILE)
+    .flatMap(loadConfig)
 }
 
 function loadConfig(configFile) {
@@ -15,10 +18,8 @@ function loadConfig(configFile) {
     .map(yaml.safeLoad)
 }
 
-function pollConfig(interval) {
-  return Bacon.interval(CONFIG_POLL_INTERVAL_SEC * 1000, CONFIG_FILE)
-    .merge(Bacon.later(0, CONFIG_FILE))
-    .flatMap(loadConfig)
+function expandTilde(path) {
+  return path.replace(/^~($|\/|\\)/, `${os.homedir()}$1`)
 }
 
 module.exports = pollConfig
