cc3e3e4eaa2603a9435e51469af25cbcddaf11b0
Heikki Pora <heikki.pora@gmail.com>
2018-01-28 12:24:41 +0200

    Verify pipeline data exactly

--- test/gitlab-integration.js
+++ test/gitlab-integration.js
@@ -36,8 +36,36 @@ describe('Gitlab client', () => {
   it('Should find latest pipeline for project with stages and retried jobs merged to one entry', async () => {
     const config = {gitlab}
     const pipelines = await fetchLatestPipelines(5290928, config)
-    expect(pipelines).to.have.lengthOf(1)
-    expect(pipelines[0].stages.test).to.have.lengthOf(1)
-    expect(pipelines[0].stages.build).to.have.lengthOf(1)
+    expect(pipelines).to.deep.equal(
+      [{
+        id: 16728199,
+        status: 'success',
+        commit: {
+          title: 'Fail more',
+          author: 'Heikki Pora'
+        },
+        ref: 'master',
+        stages: {
+          test: [
+            {
+              id: 49858895,
+              status: 'success',
+              name: 'fail_randomly_long_name',
+              startedAt: '2018-01-28T10:18:21.150Z',
+              finishedAt: '2018-01-28T10:20:10.144Z'
+            }
+          ],
+          build: [
+            {
+              id: 49858384,
+              status: 'success',
+              name: 'build_my_stuff',
+              startedAt: '2018-01-28T10:20:10.340Z',
+              finishedAt: '2018-01-28T10:21:40.299Z'
+            }
+          ]
+        }
+      }]
+    )
   })
 });
