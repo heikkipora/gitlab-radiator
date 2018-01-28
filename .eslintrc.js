2b64ff880749a966095f98a3fcfcddb1ef5862d9
Heikki Pora <heikki.pora@gmail.com>
2018-01-28 12:31:49 +0200

    Run eslint with travis

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "env": {
         "browser": true,
         "es6": true,
+        "mocha": true,
         "node": true
     },
     "extends": ["eslint:recommended", "plugin:react/recommended"],
