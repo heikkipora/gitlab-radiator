94cc2f85b788df3d9513a252bdd617edb56fabff
Heikki Pora <heikki.pora@gmail.com>
2018-02-02 15:29:59 +0200

    Add support for configurable basic-auth

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import authenticate from 'basic-auth'
 import compression from 'compression'
 import {config} from './config'
 import express from 'express'
@@ -17,6 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.disable('x-powered-by')
 app.use(compression())
+app.use(basicAuth(config.auth))
 app.use(express.static(`${__dirname}/../public`))
 
 httpServer.listen(config.port, () => {
@@ -51,5 +53,25 @@ function withDate(state) {
   return {
     ...state,
     now: Date.now()
+  }
+}
+
+function basicAuth(auth) {
+  if (!auth || !auth.username || !auth.password) {
+    // eslint-disable-next-line no-console
+    console.log('No authentication configured')
+    return (req, res, next) => next()
+  }
+
+  // eslint-disable-next-line no-console
+  console.log('HTTP basic auth enabled')
+  return (req, res, next) => {
+    const {name, pass} = authenticate(req) || {}
+    if (auth.username === name && auth.password === pass) {
+      next()
+    } else {
+      res.setHeader('WWW-Authenticate', 'Basic realm="gitlab-radiator"')
+      res.status(401).end()
+    }
   }
 }
