aaf1fd570b5faf8f734ebfb6a7c861d7797eb54b
Heikki Pora <heikki.pora@gmail.com>
2023-01-13 08:00:51 +0200

    Simplify timestamp processing
    - formatDistance() takes numbers directly
    - extract function parseDate()

--- src/client/timestamp.tsx
+++ src/client/timestamp.tsx
@@ -4,7 +4,6 @@ import type {Stage} from './gitlab-types'
 
 export function Timestamp({stages, now}: {stages: Stage[], now: number}) {
   const timestamps = getTimestamps(stages)
-
   if (timestamps.length === 0) {
     return <span>Pending...</span>
   }
@@ -12,30 +11,27 @@ export function Timestamp({stages, now}: {stages: Stage[], now: number}) {
   const finished = timestamps
     .map(t => t.finishedAt)
     .filter((t): t is number => t !== null)
+
   const inProgress = timestamps.length > finished.length
   if (inProgress) {
-    const [timestamp] = timestamps.sort((a, b) => a.startedAt - b.startedAt)
-    return <span>Started {formatDate(timestamp.startedAt, now)} ago</span>
+    const [{startedAt}] = timestamps.sort((a, b) => a.startedAt - b.startedAt)
+    return <span>Started {formatDistance(startedAt, now)} ago</span>
   }
 
   const [latestFinishedAt] = finished.sort((a, b) => b - a)
-  return <span>Finished {formatDate(latestFinishedAt, now)} ago</span>
+  return <span>Finished {formatDistance(latestFinishedAt, now)} ago</span>
 }
 
 function getTimestamps(stages: Stage[]): {startedAt: number, finishedAt: number | null}[] {
   return stages
     .flatMap(s => s.jobs)
-    .map(job => {
-      const startedAt = job.startedAt ? new Date(job.startedAt).valueOf() : null
-      const finishedAt = job.finishedAt ? new Date(job.finishedAt).valueOf() : null
-      return {
-        startedAt,
-        finishedAt
-      }
-    })
+    .map(job => ({
+      startedAt: parseDate(job.startedAt),
+      finishedAt: parseDate(job.finishedAt)
+    }))
     .filter((t): t is {startedAt: number, finishedAt: number | null} => t.startedAt !== null)
 }
 
-function formatDate(timestamp: number, now: number) {
-  return formatDistance(new Date(timestamp), new Date(now))
+function parseDate(value: string | null) {
+  return value ? new Date(value).valueOf() : null
 }
