67b328bb4d7428f1c95fa8140c291a211c27e8b3
Heikki Pora <heikki.pora@gmail.com>
2026-01-04 14:47:52 +0200

    Simplify GroupedProjects component

--- src/client/groupedProjects.tsx
+++ src/client/groupedProjects.tsx
@@ -5,18 +5,14 @@ import type {Project, ProjectsOrder} from '../common/gitlab-types'
 
 export function GroupedProjects({projects, projectsOrder, groupSuccessfulProjects, zoom, columns, now, screen}: {projects: Project[], projectsOrder: ProjectsOrder[], groupSuccessfulProjects: boolean, zoom: number, columns: number, now: number, screen: {id: number, total: number}}) {
   if (groupSuccessfulProjects) {
-    return renderProjectsGrouped(projects, projectsOrder, zoom, columns, now, screen)
+    const successfullProjects = projects.filter(p => p.status === 'success')
+    const otherProjects= projects.filter(p => p.status !== 'success')
+    const groupedProjects = Object.groupBy(successfullProjects, p => p.group)
+
+    return <React.Fragment>
+      <Projects now={now} zoom={zoom} columns={columns} projects={otherProjects} projectsOrder={projectsOrder} screen={screen}/>
+      <Groups now={now} zoom={zoom} columns={columns} groupedProjects={groupedProjects}/>
+    </React.Fragment>
   }
   return <Projects now={now} zoom={zoom} columns={columns} projects={projects} projectsOrder={projectsOrder} screen={screen}/>
-}
-
-function renderProjectsGrouped(projects: Project[], projectsOrder: ProjectsOrder[], zoom: number, columns: number, now: number, screen: {id: number, total: number}) {
-  const successfullProjects = projects.filter(({status}) => status === 'success')
-  const otherProjects= projects.filter(({status}) => status !== 'success')
-  const groupedProjects = Object.groupBy(successfullProjects, p => p.group)
-
-  return <React.Fragment>
-    <Projects now={now} zoom={zoom} columns={columns} projects={otherProjects} projectsOrder={projectsOrder} screen={screen}/>
-    <Groups now={now} zoom={zoom} columns={columns} groupedProjects={groupedProjects}/>
-  </React.Fragment>
 }
